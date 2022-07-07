b7dc94d16835ca171a504309035b833ce902307d
Данил <dan.moskalets@gmail.com>
2022-07-07 21:16:47 +0300

    feat: divided the layout into components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import './scss/app.scss'
+import Header from "./Components/Header";
+import Categories from "./Components/Categories";
+import Sort from "./Components/Sort";
+import FullPizza from "./Components/FullPizza";
 
-function App() {
-  return (
-    <p> Hello World </p>
-  );
+const App = () => {
+    return (
+        <div className="wrapper">
+           <Header/>
+            <div className="content">
+                <div className="container">
+                    <div className="content__top">
+                        <Categories/>
+                       <Sort/>
+                    </div>
+                    <FullPizza/>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
