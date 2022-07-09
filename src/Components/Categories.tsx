d9cdb1d666f988db6873fa351272e916c00d0ee4
Данил <dan.moskalets@gmail.com>
2022-07-09 19:06:43 +0300

    feat: made an array of categories and implemented witch map and use state

--- src/Components/Categories.tsx
+++ src/Components/Categories.tsx
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { useState} from 'react';
+import {v1} from "uuid";
 
 const Categories = () => {
+    const [activeIndex, setActiveIndex] = useState<number>(0)
+
+    let categoriesName = [
+        {name: 'Все', id: v1()},
+        {name: 'Мясные', id: v1()},
+        {name: 'Вегетарианская', id: v1()},
+        {name: 'Гриль', id: v1()},
+        {name: 'Острые', id: v1()},
+        {name: 'Закрытые', id: v1()},
+    ]
+
+    const changeActiveIndex = (index: number) => {
+        setActiveIndex(index)
+    }
+
     return (
         <div className="categories">
             <ul>
-                <li className="active">Все</li>
-                <li>Мясные</li>
-                <li>Вегетарианская</li>
-                <li>Гриль</li>
-                <li>Острые</li>
-                <li>Закрытые</li>
+                {categoriesName.map((categories, index) => {
+                        return (
+                            <li
+                                key={categories.id} onClick={() => changeActiveIndex(index)}
+                                className={activeIndex === index ? 'active' : ''}> {categories.name}
+                            </li>
+                        )
+                    }
+                  )
+                }
+
             </ul>
         </div>
     );
